1b4ab29f24c41111b89c8853bc8a8313907b396c
sunilkumar1996 <sunilrajputdev@gmail.com>
2021-05-11 19:52:32 +0530

    update code

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy 
-from flask_login import LoginManager 
+from flask_login import LoginManager
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
@@ -8,9 +9,12 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'thisismysecretkeydonotstealit'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://root:root@localhost:5432/flaskdb'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+    migrate = Migrate(app, db)
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
